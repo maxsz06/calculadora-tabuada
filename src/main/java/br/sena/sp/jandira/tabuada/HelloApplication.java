ca80db385400195d2c821e8188114fb1f2cb91e4
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2025-11-03 11:29:31 -0300

    Interface: Em andamento

--- src/main/java/br/sena/sp/jandira/tabuada/HelloApplication.java
+++ src/main/java/br/sena/sp/jandira/tabuada/HelloApplication.java
@@ -3,17 +3,67 @@ package br.sena.sp.jandira.tabuada;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
+
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        //Definir o tamanho da tela (Stage)
+        stage.setWidth(500);
+        stage.setHeight(500);
+
+        //Componente principal da tela
+        VBox root = new VBox();
+        Scene scene = new Scene(root);
+
+        //Cabeçario da tela
+        VBox header = new VBox();
+        header.setStyle("-fx-padding: 20;-fx-background-color: #153cbe");
+
+        //Adicionar um label ao header
+        Label labelTitulo = new Label("Tabuada");
+        labelTitulo.setStyle("-fx-text-fill: white; -fx-font-size: 30;-fx-font-weight: Bold");
+
+
+        Label labelSubTitulo = new Label("Construa tabuada sem limites!");
+        labelSubTitulo.setStyle("-fx-text-fill: white; -fx-font-size: 15");
+
+
+        HBox multiplicandoBox = new HBox();
+        multiplicandoBox.setStyle("-fx-padding: 10");
+        Label labelMultiplicando = new Label("Multiplicando");
+        TextField textFieldMultiplicando = new TextField();
+
+
+        multiplicandoBox.getChildren().add(labelMultiplicando);
+        multiplicandoBox.getChildren().add(textFieldMultiplicando);
+
+
+
+
+        //(Importante) Colocar a ordem certa de como os textos vão ficar
+
+        header.getChildren().add(labelTitulo);
+        root.getChildren().add(header);
+
+        //Colocar o header no root
+        header.getChildren().add(labelSubTitulo);
+
+        //colocamos o multplicandoBox no root
+        root.getChildren().add(multiplicandoBox);
+
+
+
         stage.setScene(scene);
+        stage.setTitle("Tabuada");
         stage.show();
     }
+
 }
--- src/main/java/br/sena/sp/jandira/tabuada/Launcher.java
+++ src/main/java/br/sena/sp/jandira/tabuada/Launcher.java
@@ -5,5 +5,11 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
+
+
+
+
     }
+
+
 }
