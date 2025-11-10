b881445e016ccf90d265548277024560ee1b9044
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2025-11-10 11:30:23 -0300

    Projeto Finalizado

--- src/main/java/br/sena/sp/jandira/tabuada/HelloApplication.java
+++ src/main/java/br/sena/sp/jandira/tabuada/HelloApplication.java
@@ -3,8 +3,11 @@ package br.sena.sp.jandira.tabuada;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -12,6 +15,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    TextField textFieldMultiplicando;
+    TextField textFieldMenorMultiplicador;
+    TextField textFieldMaiorMultplicador;
+    ListView listaTabuada = new ListView();
+
 
     public void start(Stage stage) throws IOException {
 
@@ -25,7 +34,7 @@ public class HelloApplication extends Application {
 
         //Cabeçario da tela
         VBox header = new VBox();
-        header.setStyle("-fx-padding: 20;-fx-background-color: #153cbe");
+        header.setStyle("-fx-padding: 20;-fx-background-color: #2144ad");
 
         //Adicionar um label ao header
         Label labelTitulo = new Label("Tabuada");
@@ -35,15 +44,24 @@ public class HelloApplication extends Application {
         Label labelSubTitulo = new Label("Construa tabuada sem limites!");
         labelSubTitulo.setStyle("-fx-text-fill: white; -fx-font-size: 15");
 
-
-        HBox multiplicandoBox = new HBox();
-        multiplicandoBox.setStyle("-fx-padding: 10");
+        GridPane gridFormulario = new GridPane();
         Label labelMultiplicando = new Label("Multiplicando");
-        TextField textFieldMultiplicando = new TextField();
+        textFieldMultiplicando= new TextField();
+
+        Label labelMenorMultiplicador = new Label("Menor Multiplicador:");
+        textFieldMenorMultiplicador = new TextField();
+
+        Label labelMaiorMultplicador = new Label("Maior Multiplicador:");
+        textFieldMaiorMultplicador = new TextField();
 
 
-        multiplicandoBox.getChildren().add(labelMultiplicando);
-        multiplicandoBox.getChildren().add(textFieldMultiplicando);
+        gridFormulario.add(labelMultiplicando, 0, 0);
+        gridFormulario.add(textFieldMultiplicando, 1, 0);
+        gridFormulario.add(labelMenorMultiplicador, 0, 1);
+        gridFormulario.add(textFieldMenorMultiplicador, 1, 1);
+        gridFormulario.add(labelMaiorMultplicador, 0, 2);
+        gridFormulario.add(textFieldMaiorMultplicador, 1, 2);
+
 
 
 
@@ -53,17 +71,81 @@ public class HelloApplication extends Application {
         header.getChildren().add(labelTitulo);
         root.getChildren().add(header);
 
-        //Colocar o header no root
+        //Criar o componente de botões
+
+        HBox boxBotoes  = new HBox();
+        Button btnCalcular = new Button("Calcular");
+        btnCalcular.setOnAction(event -> {
+            calcularTabuada();
+        });
+
+        Button btnLimpar = new Button("Limpar");
+        Button btnSair = new Button("Sair");
+
+        //Adicionar os botões na boxBotoes
+        boxBotoes.getChildren().addAll(btnCalcular, btnLimpar, btnSair);
+
+        //Adicionar um componente ListView
+        VBox boxResultado = new VBox(boxBotoes);
+        Label labelResultado = new Label("Resultado:");
+        labelResultado.setStyle("-fx-text-fill: blue; -fx-font-size: 18;-fx-font-weight: bold");
+
+
+        // Adicionar o ListView
+
+      listaTabuada = new ListView();
+
+
+
+        //Adicionar o label ao box resultado
+
+        boxResultado.getChildren().add(labelResultado);
+        boxResultado.getChildren().add(listaTabuada);
+
+
+        //Adicionar componentes ao root
         header.getChildren().add(labelSubTitulo);
-
-        //colocamos o multplicandoBox no root
-        root.getChildren().add(multiplicandoBox);
-
+        root.getChildren().add(gridFormulario);
+        root.getChildren().add(boxBotoes);
+        root.getChildren().add(boxResultado);
 
 
         stage.setScene(scene);
         stage.setTitle("Tabuada");
         stage.show();
+    }
+
+    public void calcularTabuada() {
+
+        int multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
+        int  menorMultiplicador = Integer.parseInt(textFieldMenorMultiplicador.getText());
+        int maiorMultiplicador = Integer.parseInt(textFieldMaiorMultplicador.getText());
+
+        if (menorMultiplicador > maiorMultiplicador) {
+
+            int auxiliar = menorMultiplicador;
+            menorMultiplicador = maiorMultiplicador;
+            maiorMultiplicador = auxiliar;
+        }
+
+        int tamanhoVetor= maiorMultiplicador - menorMultiplicador +1;
+        String[] tabuada = new String[tamanhoVetor];
+
+
+        int contador = 0;
+
+        while (contador < tamanhoVetor) {
+
+            double produto = multiplicando * menorMultiplicador;
+
+            tabuada[contador] = multiplicando + " X " + menorMultiplicador + " = "  + produto;
+            contador++;
+            menorMultiplicador ++;
+        }
+
+        listaTabuada.getItems().clear();
+        listaTabuada.getItems().addAll(tabuada);
+
     }
 
 }
